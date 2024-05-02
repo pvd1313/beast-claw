f7bb5c37b61225e2e7e039898d2c45da3a71614d
v.fuchadzhy <pvd1313@gmail.com>
2024-05-02 16:26:29 +0300

    Add project files

---
+++ Runtime/Source/Data/GUIStylesConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace RisenBeast.Claw
+{
+    internal static class GUIStylesConfig
+    {
+        public const string SearchField = "SearchTextField";
+        public const string ListItemNormal = "textfield";
+        public const string ListItemSelected = "CN CountBadge";
+    }
+}
---
+++ Runtime/Source/Data/TypeInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RisenBeast.Claw
+{
+    internal struct TypeInfo
+    {
+        public Type Type;
+        public string NameShort;
+        public string NameShortLowerInvariant;
+        public string Namespace;
+    }
+}
